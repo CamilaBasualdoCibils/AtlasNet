b2ce7beb2ab1fa885810b5bc737644ae4f19aa95
Camila Basualdo Cibils <camila.basualdocibils@gmail.com>
2026-01-20 18:44:03 +0000

    fixed compilation mostly

--- AtlasNet/apps/AtlasNet/src/Utils.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <filesystem>
-
-std::filesystem::path GetEXEPath();
-std::filesystem::path GetSDKPath();
---
+++ AtlasNet2/start/AtlasNet/src/Utils.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <filesystem>
+
+std::filesystem::path GetEXEPath();
+std::filesystem::path GetSDKPath();
+std::filesystem::path GetAtlasNetPath();
