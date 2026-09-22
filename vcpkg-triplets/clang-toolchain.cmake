8cfc34a2c09d333c6e21a0cbde4fe12563273d9b
CJ Osborne <cj.l.osborne@gmail.com>
2026-09-21 19:51:30 -0700

    Use GCC 14 for C++23 CI builds

--- vcpkg-triplets/clang-toolchain.cmake
+++ vcpkg-triplets/clang-toolchain.cmake
@@ -11,9 +11,3 @@ else()
   set(CMAKE_C_COMPILER /usr/bin/clang)
   set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 endif()
-
-# Use libc++ explicitly so C++23 library facilities such as std::expected are
-# available consistently on CI and local Clang installations.
-set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-stdlib=libc++")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-stdlib=libc++")
